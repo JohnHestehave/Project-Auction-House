4aa1caee4730b71e351cfe28eeb26c24c77cfda5
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2017-08-23 12:08:29 +0200

    send client list

--- Project Auction House Client/Client.xaml.cs
+++ Project Auction House Client/Client.xaml.cs
@@ -15,73 +15,60 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Project_Auction_House_Client
-{
-	/// <summary>
-	/// Interaction logic for Client.xaml
-	/// </summary>
-	public partial class Client : Window
-	{
+namespace Project_Auction_House_Client {
+    /// <summary>
+    /// Interaction logic for Client.xaml
+    /// </summary>
+    public partial class Client:Window {
 
-		TcpClient server;
-		NetworkStream stream;
-		StreamReader sr;
-		StreamWriter sw;
-		public Client()
-		{
-			InitializeComponent();
-		}
-		
-		public bool Connect(string ip)
-		{
-			try
-			{
-				server = new TcpClient(ip, 12345);
-				stream = server.GetStream();
-				sr = new StreamReader(stream);
-				sw = new StreamWriter(stream);
-				sw.AutoFlush = true;
-				return true;
-			}
-			catch(Exception)
-			{
-				return false;
-			}
-		}
+        TcpClient server;
+        NetworkStream stream;
+        StreamReader sr;
+        StreamWriter sw;
+        public Client() {
+            InitializeComponent();
+        }
 
-		public void Start()
-		{
-			Thread thread = new Thread(Loop);
-			thread.Start();
-		}
+        public bool Connect(string ip) {
+            try {
+                server = new TcpClient(ip, 12345);
+                stream = server.GetStream();
+                sr = new StreamReader(stream);
+                sw = new StreamWriter(stream);
+                sw.AutoFlush = true;
+                return true;
+            } catch (Exception) {
+                return false;
+            }
+        }
 
-		public delegate void UpdateText(string message);
-		public void UpdateText2(string message)
-		{
-			ServerText.Text += "\n"+message;
-		}
-		public void Loop()
-		{
-			while (true)
-			{
-				string message = sr.ReadLine();
-				ServerText.Dispatcher.Invoke(new UpdateText(UpdateText2), message);
-				continue;
-				switch (message)
-				{
-					case "test":
-						ServerText.Text += "\n" + "Test received." + message;
-						break;
-					default:
-						ServerText.Text += "\n" + message;
-						break;
-				}
-				if (!server.Connected)
-				{
-					ServerText.Text += "\n" + "Disconnected.";
-					break;
-				}
-			}
-		}
-	}
+        public void Start() {
+            Thread thread = new Thread(Loop);
+            thread.Start();
+        }
+
+        public delegate void UpdateText(string message);
+        public void UpdateText2(string message) {
+            ServerText.Text = "\n" + message;
+        }
+        public void Loop() {
+            while (true) {
+                string message = sr.ReadLine();
+                ServerText.Dispatcher.Invoke(new UpdateText(UpdateText2), message);
+                continue;
+                switch (message) {
+                    case "test":
+                        ServerText.Text += "\n" + "Test received." + message;
+                        break;
+                    default:
+                        ServerText.Text += "\n" + message;
+                        break;
+                }
+                if (!server.Connected) {
+                    ServerText.Text += "\n" + "Disconnected.";
+                    break;
+                }
+            }
+        }
+    }
 }
--- Project Auction House Server/Program.cs
+++ Project Auction House Server/Program.cs
@@ -16,27 +16,23 @@ namespace Project_Auction_House_Server {
 
         int Bid = 0;
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             Program Run = new Program();
             Run.Run();
         }
 
-        private void Run()
-        {
+        private void Run() {
             TcpListener Server = new TcpListener(IPAddress.Any, 12345);
             TcpClient Client;
 
             Server.Start();
 
-            while (true)
-            {
+            while (true) {
                 Client = Server.AcceptTcpClient();
                 ThreadPool.QueueUserWorkItem(ClientConnection, Client);
             }
         }
-        private void ClientConnection(object obj)
-        {
+        private void ClientConnection(object obj) {
             var client = (TcpClient)obj;
 
             StreamReader read = new StreamReader(client.GetStream());
@@ -47,25 +43,37 @@ namespace Project_Auction_House_Server {
             Console.WriteLine("New Connection: " + IPEP);
 
             SendFTInfo(client);
-
+            showBidders();
 
         }
-        private void Broadcast()
-        {
+
+        private void showBidders() {
+            string message = "";
+            foreach (var clientIp in ClientIPS) {
+                IPEndPoint ipep = (IPEndPoint)clientIp.Client.RemoteEndPoint;
+                message += ipep.Address.ToString() + "\n";
+            }
+
+            foreach (var client in ClientIPS) {
+                StreamWriter write = new StreamWriter(client.GetStream());
+                write.AutoFlush = true;
+                write.WriteLine(message);
+            }
+        }
+
+        private void Broadcast() {
             Console.WriteLine("Broadcasting...");
-            foreach (var client in ClientIPS)
-            {
+            foreach (var client in ClientIPS) {
                 StreamWriter write = new StreamWriter(client.GetStream());
                 write.AutoFlush = true;
                 write.WriteLine("test1");
             }
         }
-        private void SendFTInfo(TcpClient client)
-        {
+        private void SendFTInfo(TcpClient client) {
             StreamWriter write = new StreamWriter(client.GetStream());
             write.AutoFlush = true;
             Console.WriteLine("Sending Current Auction to New Client");
-            write.WriteLine("test2");
+            
         }
     }
 }
