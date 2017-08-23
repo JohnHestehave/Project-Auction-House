77ea86655d35324cf8a326dc8c8dcda7a826a52d
John H <johnh.dk93@yahoo.dk>
2017-08-23 11:18:59 +0200

    update numero uno

---
+++ Project Auction House Client/Client.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_Auction_House_Client
+{
+	/// <summary>
+	/// Interaction logic for Client.xaml
+	/// </summary>
+	public partial class Client : Window
+	{
+
+		TcpClient server;
+		NetworkStream stream;
+		StreamReader sr;
+		StreamWriter sw;
+		public Client()
+		{
+			InitializeComponent();
+		}
+		
+		public bool Connect(string ip)
+		{
+			try
+			{
+				server = new TcpClient(ip, 12345);
+				stream = server.GetStream();
+				sr = new StreamReader(stream);
+				sw = new StreamWriter(stream);
+				sw.AutoFlush = true;
+				return true;
+			}
+			catch(Exception)
+			{
+				return false;
+			}
+		}
+
+		public void Start()
+		{
+			Thread thread = new Thread(Loop);
+			thread.Start();
+		}
+
+		public delegate void UpdateText(string message);
+		public void UpdateText2(string message)
+		{
+			ServerText.Text += "\n"+message;
+		}
+		public void Loop()
+		{
+			while (true)
+			{
+				string message = sr.ReadLine();
+				ServerText.Dispatcher.Invoke(new UpdateText(UpdateText2), message);
+				continue;
+				switch (message)
+				{
+					case "test":
+						ServerText.Text += "\n" + "Test received." + message;
+						break;
+					default:
+						ServerText.Text += "\n" + message;
+						break;
+				}
+				if (!server.Connected)
+				{
+					ServerText.Text += "\n" + "Disconnected.";
+					break;
+				}
+			}
+		}
+	}
+}
--- Project Auction House Client/MainWindow.xaml.cs
+++ Project Auction House Client/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,32 @@ namespace Project_Auction_House_Client
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Login(object sender, RoutedEventArgs e)
+		{
+			string ip = IPTextBox.Text;
+
+			Regex regex = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+			MatchCollection matches = regex.Matches(ip);
+			if(matches.Count > 0)
+			{
+				foreach (Match match in matches)
+				{
+					ip = match.Value;
+					Client c = new Client();
+					if (c.Connect(ip))
+					{
+						c.Show();
+						c.Start();
+						this.Close();
+						break;
+					}
+				}
+			}else
+			{
+				MessageBox.Show("Wrong IP");
+			}
 		}
 	}
 }
