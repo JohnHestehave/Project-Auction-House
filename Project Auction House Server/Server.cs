36c192382f86d737e6c2357f7574901d6b66a98e
andr9528 <andr9528@edu.eal.dk>
2017-09-01 10:06:23 +0200

    refactored server into different classes, should technically work

--- Project Auction House Server/Server.cs
+++ Project Auction House Server/Server.cs
@@ -9,17 +9,11 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Project_Auction_House_Server { // Split different Methods out to different Classes, to improve Design and readability.
-    class Server {
+
+     
+    public class Server {
 
         // ¤ is used only to places meant to be split!
-
-        List<string> BidLog = new List<string>();
-        static List<TcpClient> ClientIPS = new List<TcpClient>();
-        static List<Item> items = new List<Item>();
-
-        int AuctionItem = 0;
-
-        int Bid = 0; // add locks to all places that tries to change this
 
         static void Main(string[] args) {
             Server Run = new Server();
@@ -29,18 +23,18 @@ namespace Project_Auction_House_Server { // Split different Methods out to diffe
         private void Run() {
             Console.WriteLine("Online");
 
-            items.Add(new Item("Ferrari 250 GTO", 130000000, "car"));
-            items.Add(new Item("Fiat Multipla", 20000, "world ugliest car"));
-            items.Add(new Item("Arne Jacobsen Svanen", 15000, "Armchair"));
-            items.Add(new Item("Rolex Submariner", 120000, "18 karat gold"));
-            items.Add(new Item("Poul Henningsen: Koglen",35000 , "Pendel"));
-            items.Add(new Item("Gaffatape", 100 , "100m"));
-            items.Add(new Item("Shure Sm7b", 3000 , "Microphone"));
-            items.Add(new Item("Gold", 260000 , "1kg"));
-            items.Add(new Item("Uran", 9500 , "4g"));
-            items.Add(new Item("Garden fertilizer", 90 , "7.5kg"));
+            Repo.Items.Add(new Item("Ferrari 250 GTO", 130000000, "car"));
+            Repo.Items.Add(new Item("Fiat Multipla", 20000, "world ugliest car"));
+            Repo.Items.Add(new Item("Arne Jacobsen Svanen", 15000, "Armchair"));
+            Repo.Items.Add(new Item("Rolex Submariner", 120000, "18 karat gold"));
+            Repo.Items.Add(new Item("Poul Henningsen: Koglen",35000 , "Pendel"));
+            Repo.Items.Add(new Item("Gaffatape", 100 , "100m"));
+            Repo.Items.Add(new Item("Shure Sm7b", 3000 , "Microphone"));
+            Repo.Items.Add(new Item("Gold", 260000 , "1kg"));
+            Repo.Items.Add(new Item("Uran", 9500 , "4g"));
+            Repo.Items.Add(new Item("Garden fertilizer", 90 , "7.5kg"));
 
-            Bid = items[AuctionItem].startPrice;
+            Repo.Bid = Repo.Items[Repo.AuctionItem].startPrice;
             
             TcpListener Server = new TcpListener(IPAddress.Any, 12345);
             TcpClient Client;
@@ -53,117 +47,25 @@ namespace Project_Auction_House_Server { // Split different Methods out to diffe
             Console.WriteLine("Offline");
             Console.ReadKey();
         }
-        private void ClientConnection(object obj) { // ClientHandler Class
+        private void ClientConnection(object obj) { // Refactor into a ClientHandler Class
             var client = (TcpClient)obj;
-            
 
-            StreamReader read = new StreamReader(client.GetStream());
-            StreamWriter write = new StreamWriter(client.GetStream());
-            write.AutoFlush = true;
-            
-            IPEndPoint IPEP = (IPEndPoint)client.Client.RemoteEndPoint;
+            ClientHandler handler = new ClientHandler(client);
 
-            ClientIPS.Add(client);
+            Repo.Clients.Add(handler);
 
-            Console.WriteLine("New Connection: " + IPEP);
-            Console.WriteLine(ClientIPS.Count + " Client(s) Connected");
+            Console.WriteLine("New Connection: " + handler.IP());
+            Console.WriteLine(Repo.Clients.Count + " Client(s) Connected");
 
-            SendFTInfo(client);
-            ShowBidders();
-
-            while (true)
-            {
-                try
-                {
-                    string message = read.ReadLine();
-                    bool conversion;
-                    int BidAttempt = 0;
-
-                    if (message != "" && message != "EXIT")
-                    {
-                        BidLog.Add(message);
-                        //TestPrint();
-                        conversion = int.TryParse(message, out BidAttempt);
-                    }
-                    if (BidAttempt < 0)
-                    {
-                        write.WriteLine("Can't Bid under Zero!");
-                    }
-                    else if (BidAttempt <= Bid)
-                    {
-                        int minimumBid = Bid + 1;
-                        write.WriteLine("Too low of a Bid, Needs to be atleast " + minimumBid);
-                    }
-                    else if (BidAttempt > Bid)
-                    {
-                        Bid = BidAttempt;
-                        write.WriteLine("You Are the Highest Bidder");
-                        Broadcast();
-                    }
-                    if (!client.Connected || message == "EXIT")
-                    {
-                        if (message == "EXIT")
-                        {
-                            
-                        }
-                        ClientIPS.Remove(client);
-                        Console.WriteLine(IPEP + " - Disconnected");
-                        Console.WriteLine(ClientIPS.Count + " Client(s) Connected");
-                        break;
-                    }
-                }
-                catch (Exception e)
-                {
-                    if (e.GetType() == typeof(IOException))
-                    {
-                        client.Close();
-                        ClientIPS.Remove(client);
-                        Console.WriteLine(IPEP + " - Terminated");
-                        Console.WriteLine(ClientIPS.Count + " Client(s) Connected");
-                        break;
-                    }
-                    
-                }
-
-                
-            }
+            handler.Handle();
         }
 
         private void TestPrint()
         {
-            foreach (var log in BidLog)
+            foreach (var log in Repo.BidLog)
             {
                 Console.WriteLine(log);
             }
-        }
-
-        private void ShowBidders() {
-            string message = "";
-            foreach (var clientIp in ClientIPS) {
-                IPEndPoint ipep = (IPEndPoint)clientIp.Client.RemoteEndPoint;
-                message += ipep.Address.ToString() + "¤";
-            }
-
-            foreach (var client in ClientIPS) {
-                StreamWriter write = new StreamWriter(client.GetStream());
-                write.AutoFlush = true;
-                write.WriteLine(message);
-            }
-        }
-
-        private void Broadcast() {
-            Console.WriteLine("Broadcasting...");
-            foreach (var client in ClientIPS) {
-                StreamWriter write = new StreamWriter(client.GetStream());
-                write.AutoFlush = true;
-                write.WriteLine(Bid);
-            }
-        }
-        private void SendFTInfo(TcpClient client) {
-            StreamWriter write = new StreamWriter(client.GetStream());
-            write.AutoFlush = true;
-            Console.WriteLine("Sending Current Auction to New Client");
-            write.WriteLine(items[AuctionItem].startPrice.ToString() + "¤" + items[AuctionItem].name.ToString() + "¤" + items[AuctionItem].description.ToString());
         }
     }
 }
